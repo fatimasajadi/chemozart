70c73e42f3f0edf37e3d33d1bda6ea762e057da4
mohamad <netfars@gmail.com>
2015-03-18 21:15:46 +0330

    fixed: Better background color for modals

--- client/app/services/storage.js
+++ client/app/services/storage.js
@@ -2,7 +2,38 @@
 
 angular.module('chemartApp')
   .factory('storage', function () {
-    var storage = {
+    var data = localStorage.molecules || {};
+    var current;
 
+    var storage = {
+      "new": function () {
+        var date = new Date();
+        var name = 'Untitled-' + date.toISOString();
+        data[name] = [];
+
+        this.setCurrent(name);
+      },
+      "save": function (value) {
+        if(value && current) {
+          data[current] = value;
+        }
+
+        localStorage.molecules = data;
+      },
+      "get": function (name) {
+        return data[name];
+      },
+      "rename": function (oldName, newName) {
+        data[newName] = data[oldName];
+        delete data[oldName];
+
+        this.save();
+      },
+      "setCurrent": function (name) {
+        current = name;
+      }
     };
+
+    return storage;
+
   });
