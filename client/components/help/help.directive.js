6aa4fcae8bf3539ea87b7de2efb1f67a8e8ee668
mohamad <netfars@gmail.com>
2015-03-18 20:09:51 +0330

    added: Add close button to help directive

--- client/components/help/help.directive.js
+++ client/components/help/help.directive.js
@@ -4,7 +4,7 @@ angular.module('chemartApp')
   .directive('help', function () {
     return {
       restrict: 'E',
-      link: function (scope, elem, attr) {
+      link: function (scope, elem) {
         elem.on('click', function () {
           elem.removeClass('show').addClass('hide');
           setTimeout(function () {
