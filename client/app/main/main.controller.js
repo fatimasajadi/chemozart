6a997eae0f6f459a8b6fb09eb0fca01a081234cf
mohamad <netfars@gmail.com>
2015-03-22 13:25:47 +0430

    fixed: Make main.controller more thin and use services instead

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,46 +1,31 @@
 'use strict';
 
 angular.module('chemartApp')
-  .controller('MainCtrl', function ($scope, $http, moleculeDrawer, canvas, storage) {
+  .controller('MainCtrl', function ($scope, $http, $download, moleculeDrawer, canvas, storage, builder) {
 
+    if (canvas === null) {
+      return;
+    }
+
+    // Assign scope values
     $scope.canvas = canvas;
     $scope.storage = storage;
-    storage.new();
-
+    $scope.builder = builder;
     $scope.modes = Mol3D.Mode;
     $scope.displays = Mol3D.Display;
+
+    // Create a new file
+    storage.new();
+
+    // Set mode
     $scope.mode = function (mode) {
       canvas.setMode(mode);
     };
 
+    // Start draw and redrawing
     canvas.show();
 
     var container = document.getElementById('canvas-container');
-
     angular.element(container).append(canvas.renderer.domElement);
-
-    $scope.build = function () {
-      var molecule = canvas.getMolecule();
-      $http.post('/api/geometry/build', molecule.toJSON()).success(function (data) {
-        var mol = Chem.Molecule.readJSON(data);
-        moleculeDrawer(mol);
-      });
-    };
-
-    $scope.addHydrogens = function () {
-      var molecule = canvas.getMolecule();
-      $http.post('/api/geometry/addhydrogens', molecule.toJSON()).success(function (data) {
-        var mol = Chem.Molecule.readJSON(data);
-        moleculeDrawer(mol);
-      });
-    };
-
-    $scope.help = function () {
-      angular.element(document.querySelector('help')).addClass('show');
-    };
-
-    $scope.about = function () {
-      angular.element(document.querySelector('about')).addClass('show');
-    };
 
   });
