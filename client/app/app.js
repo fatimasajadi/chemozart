c07bdaf51b0af7e98184ba9de434b92e54130614
mohamad <netfars@gmail.com>
2015-03-23 17:56:18 +0430

    added: Some dependencies referred in app

--- client/app/app.js
+++ client/app/app.js
@@ -4,11 +4,19 @@ angular.module('chemartApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ui.router'
+  'ui.router',
+  'cfp.hotkeys',
+  'lr.upload',
+  'cgNotify'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
+  })
+  .run(function (notify) {
+    notify.config({
+      duration: 3000
+    });
   });
