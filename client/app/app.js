7b6f386fd6bf7dfd32ef4166fa499212d73d6ee3
Fatima Sajadi <s.sfatemeh@gmail.com>
2016-03-25 10:20:19 -0400

    Remove conversion api and make use of openbabel ecmascripten

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+window.OpenBabel = OpenBabelModule();
+
 angular.module('chemartApp', [
   'ngCookies',
   'ngResource',
--- client/app/resources/conversion.factory.js
+++ client/app/resources/conversion.factory.js
@@ -1,33 +1,51 @@
-'use strict';
+"use strict";
 
-angular.module('chemartApp')
-  .factory('conversion', function ($http, moleculeDrawer, canvas, $download, storage, notify) {
-
+angular
+  .module("chemartApp")
+  .factory("conversion", function (
+    moleculeDrawer,
+    canvas,
+    $download,
+    storage,
+    notify
+  ) {
     return {
-      "formats": {
-        "mol2": "Sybyl Mol2",
-        "cml": "CML",
-        "mol": "MDL SDFile",
-        "pdb": "PDB",
-        "smiles": "SMILES Notation",
-        "hin": "Hyperchem File"
+      formats: {
+        mol2: "Sybyl Mol2",
+        cml: "CML",
+        mol: "MDL SDFile",
+        pdb: "PDB",
+        smiles: "SMILES Notation",
+        hin: "Hyperchem File",
       },
-      "export": function (type) {
-        var molecule = canvas.getMolecule().toJSON();
-        $http.post('/api/conversion/export/' + type, molecule).success(function (data) {
-          $download(data, storage.current + '.' + type);
-        });
+      export: function (type) {
+        var molecule = JSON.parse(
+          JSON.stringify(canvas.getMolecule().toJSON())
+        );
+        var conv = new OpenBabel.ObConversionWrapper();
+        var obMol = JSONToOB(molecule);
+
+        try {
+          conv.setOutFormat("", type);
+          var outData = conv.writeString(obMol, false);
+          $download(outData, storage.current + "." + type);
+        } finally {
+          conv.delete();
+          obMol.delete();
+        }
       },
-      "import": function (file) {
+      import: function (file) {
         function getExtension(filename) {
-          return filename.substr(filename.lastIndexOf('.') + 1)
+          return filename.substr(filename.lastIndexOf(".") + 1);
         }
         var type = getExtension(file.name);
 
-        if(['cml', 'hin', 'mol', 'mol2', 'smiles', 'pdb'].indexOf(type) === -1) {
+        if (
+          ["cml", "hin", "mol", "mol2", "smiles", "pdb"].indexOf(type) === -1
+        ) {
           return notify({
-            message: 'The file selected is not supported.',
-            classes: ['error']
+            message: "The file selected is not supported.",
+            classes: ["error"],
           });
         }
 
@@ -35,21 +53,87 @@ angular.module('chemartApp')
         fr.readAsBinaryString(file);
 
         fr.onload = function (e) {
-          var data = e.target.result;
-          $http.post('/api/conversion/import/' + type, {
-            data: data
-          }).success(function (data) {
-            var mol = Chem.Molecule.readJSON(data);
+          var inData = e.target.result;
+          var conv = new OpenBabel.ObConversionWrapper(); // create ObConversionWrapper instance
+          try {
+            conv.setInFormat("", type);
+
+            var obMol = new OpenBabel.OBMol();
+            conv.readString(obMol, inData);
+
+            var json = OBToJSON(obMol);
+            var mol = Chem.Molecule.readJSON(json);
             moleculeDrawer.draw(mol);
 
             notify({
               message: 'The file "' + file.name + '" has been imported',
-              classes: ['success']
+              classes: ["success"],
             });
-          });
+          } finally {
+            obMol.delete();
+            conv.delete();
+          }
         };
-      }
-
+      },
     };
-
   });
+
+function JSONToOB(molecule) {
+  var obMol = new OpenBabel.OBMol();
+
+  var atoms = molecule.atoms.map(function (atom) {
+    var obAtom = obMol.NewAtom();
+    obAtom.SetAtomicNum(atom.atomicNumber);
+    obAtom.SetIdx(atom.index);
+    obAtom.SetVector(atom.position.x, atom.position.y, atom.position.z);
+    return obAtom;
+  });
+
+  molecule.bonds.forEach(function (bond) {
+    var obBond = obMol.NewBond();
+    obBond.SetBondOrder(bond.order);
+    obBond.SetBegin(
+      atoms.find(function (atom) {
+        return atom.GetIdx() == bond.begin;
+      })
+    );
+    obBond.SetEnd(
+      atoms.find(function (atom) {
+        return atom.GetIdx() == bond.end;
+      })
+    );
+    return obBond;
+  });
+
+  return obMol;
+}
+
+function OBToJSON(mol) {
+  var result = { atoms: [], bonds: [] };
+  var atomCount = mol.NumAtoms();
+  var bondCount = mol.NumBonds();
+
+  for (var i = 0; i < atomCount; ++i) {
+    var atom = mol.GetAtom(i + 1);
+    result.atoms.push({
+      index: atom.GetIdx(),
+      atomicNumber: atom.GetAtomicNum(),
+      position: {
+        x: atom.GetX(),
+        y: atom.GetY(),
+        z: atom.GetZ(),
+      },
+    });
+  }
+
+  for (var i = 0; i < bondCount; ++i) {
+    var bond = mol.GetBond(i + 1);
+    result.bonds.push({
+      begin: bond.GetBeginAtomIdx(),
+      end: bond.GetEndAtomIdx(),
+      order: bond.GetBondOrder(),
+    });
+  }
+
+  return result;
+}
